15eacdb1d8b3e8f1ec079e67d8810c1ac026688c
Ravi kumar <ravikrrxl123@gmail.com>
2019-09-07 15:40:11 +0530

    Again Added My code

--- CarManagement/Assignment1.java
+++ CarManagement/Assignment1.java
@@ -1,12 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1;
 
-import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
+import java.util.Scanner;
 
+/**
+ *
+ * @author ravi
+ */
 public class Assignment1 {
 
-    public static void main(String sb[]) {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+        
+       
         
         int num;
         ArrayList<Customer> list = new ArrayList<Customer>();
@@ -33,25 +50,73 @@ public class Assignment1 {
                          case 2:
                                  // call Car class and add value
                                  int counter=0;
+                                 int carId;
+                                 String carModel;
+                                 double carPrice;
+                                 
+                                 
                                  Scanner obj1=new Scanner(System.in);
+                                 Scanner obj2=new Scanner(System.in);
+                                 
                                  System.out.println("Enter Id Of Customer:");
                                  int x=obj1.nextInt();
+                                 
+                                 
                                  for(int i=0;i<list.size();i++)
                                 {
                                         if (list.get(i).getId() == x)
-                                                {
+                                                {   counter++;
+                                                 
                                                     Customer customer=list.get(i);
                                                     ArrayList<Car> listOfCars=  customer.getCarList();
-                                                    listOfCars.add(new Car());
-                                                    customer.setCarList(listOfCars);
-                                                    System.out.println("car count"+listOfCars.size());
-                                                    counter++;
-                                                 }
-                                } if(counter==0)
-                                            {
-                                            System.out.println("Id is Not Exist Need To Add New Customer.");
-                                            }
-                                break;
+                                                    Car car=null;
+                                                   
+                                                   System.out.println("Enter CarType( press Toyota: 1, Maruti: 2,hyundai: 3 )");
+                                                   int carType=obj1.nextInt();
+                                                         switch (carType) 
+                                                    {
+                                                    case 1:
+                                                            System.out.println("Enter car Id,car model and car Price");
+                                                            carId=obj1.nextInt();
+                                                            carModel=obj2.next();
+                                                            carPrice=obj1.nextDouble();
+                                                            car=new Maruti(carId,carModel,carPrice);
+                                                            listOfCars.add(car);
+                                                            customer.setCarList(listOfCars);
+                                                            break;
+                                                    case 2: System.out.println("Enter car Id,car model and car Price");
+                                                            carId=obj1.nextInt();
+                                                            carModel=obj2.next();
+                                                            carPrice=obj1.nextDouble();
+                                                            car=new Hyundai(carId,carModel,carPrice);
+                                                 
+                                                            listOfCars.add(car);
+                                                            customer.setCarList(listOfCars);
+                                                           break;
+                                                    case 3: 
+                                                        System.out.println("Enter car Id,car model and car Price");
+                                                        carId=obj1.nextInt();
+                                                        carModel=obj2.next();
+                                                        carPrice=obj1.nextDouble();
+                                                        car=new Toyota(carId,carModel,carPrice);
+                                          
+                                                        listOfCars.add(car);
+                                                        customer.setCarList(listOfCars);
+                                                           break;
+                                                    default:
+                                                           System.out.println("Car Type Does not Exist");
+                                                           break;
+                                                    
+                                                    }
+                                                   
+                                        }
+                                }
+                                 
+                                 
+                               if(counter==0)
+                                     {
+                                       System.out.println("Id is Not Exist Need To Add New Customer."); }
+                               break;
                          
                          
                          case 3:
@@ -65,31 +130,37 @@ public class Assignment1 {
                                     + list.get(i).getName() );
                                 for(Car carDetail : list.get(i).getCarList())
                                 {
-                                 System.out.println("Car Name: "+carDetail.getCarName()+" Car Type : "+carDetail.getCarType()+" Car Price: "+carDetail.getPrice());
+                                 System.out.println("Car Id: "+carDetail.getCarId() +" Car Model : "+carDetail.getCarModel()+" Car Price: "+carDetail.getCarPrice()+
+                                         " Resales Price :"+carDetail.getResalePrice());
                                  }
                                  } break;
                          case 4:
                                  // Search the Customer By Id
                              
                                     SearchById search = new SearchById();
+                                    int count=0;
                                     for (int i = 0; i < list.size(); i++)
                                     {
                                         int x1 = search.getCusId();
                                         if (list.get(i).getId() == x1)
-                                        {
-                                            System.out.println(" Name : "
+                                        {   count++;
+                                            System.out.println("Name of Customer: "
                                             + list.get(i).getName());
-                                             for(Car carDetail : list.get(i).getCarList()){
-                                           System.out.println("Car Name  "+carDetail.getCarName());
-                                           }
-                                         }
-                                    }break;
+                                             for(Car carDetail : list.get(i).getCarList())
+                                             {
+                                                    System.out.println("Car Name  "+carDetail.getCarModel());
+                                             }
+                                        }
+                                    }
+                                    if(count==0)System.out.println("Customer Id is incorrect");
+                                    
+                                    break;
                     
            
               
                         case 5:
                                   // here user Randon to distubute prize
-                    
+                                    int flag=0;
                                     Random random = new Random();
                                     RandomNumber r = new RandomNumber();
                                     randam = new int[6];
@@ -99,20 +170,25 @@ public class Assignment1 {
                                          randam[i] = list.get(index).getId();
                                      }
                                    for (int i = 0; i < 6; i++) 
-                                    {
+                                    { 
+                                      
                                         if (randam[i] == r.num1) {
+                                              flag++;
                                         System.out.println("Id : " + randam[i] + " price Winner");
                                         break;
                                         }
                                         else if (randam[i] == r.num2) {
+                                              flag++;
                                         System.out.println("Id : " + randam[i] + " price Winner");
                                           break;
                                         }
                                         else  if (randam[i] == r.num3) {
+                                              flag++;
                                         System.out.println("Id : " + randam[i] + " price Winner");
                                           break;
-                                        }
-                                    }break;
+                                        } 
+                                    } if(flag==0)System.out.println("Better Luck For next time :)");
+                                   break;
                     }
 
                  } while (num != 0);
@@ -126,7 +202,7 @@ class Customer implements Comparable< Customer> {
     public String name;
     public ArrayList<Car> carList;
 
-    Customer() {
+  public  Customer() {
         Scanner obj1 = new Scanner(System.in);
         Scanner obj2 = new Scanner(System.in);
         System.out.println("Enter ID, Name");
@@ -166,40 +242,32 @@ class Customer implements Comparable< Customer> {
     
 
 }
+// Car Class is as abstract class for different type of car
+ abstract class Car {
+     public int carId;
+     public String carModel;
+     double carPrice;
 
-class Car  extends Price{
-
-//  public int CarId;
-    public String CarType;
-    public String CarName;
   
-    Scanner obj1 = new Scanner(System.in);
-    Scanner obj2 = new Scanner(System.in);
+    public int getCarId() {
+        return carId;
+    }
 
-   public Car() {
-        System.out.println("Enter CarType( press Toyota: 1, Maruti: 2,hyundai: 3 ),CarName, CarPrice");
-        //CarId = obj1.nextInt();
-        
-        CarType = obj2.next();
-        CarName = obj2.next();
-        
-        //this.price = obj1.nextDouble();
-        setPrice(obj1.nextDouble());
-        
+    public double getCarPrice() {
+        return carPrice;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
     
-    }
-
-
-    public String getCarType() {
-        return CarType;
-    }
-
-    public String getCarName() {
-        return CarName;
-    }
+       abstract double getResalePrice();
 
 
  }
+
+
+// class is created for search the customer By id
 
 class SearchById {
 
@@ -218,7 +286,7 @@ class SearchById {
 }
 
 
-
+// this class created for distribute prize
 class RandomNumber {
 
     public int num1;
@@ -244,23 +312,56 @@ class RandomNumber {
     public int getNum3() {
         return num3;
     }
-    }
-
- abstract class Price{
     
-    double price;
-
-    public Price() {
     }
 
-    public double getPrice() {
-        return price*80/100;
+  // Hyundai class is created 
+
+class Hyundai extends Car {
+
+    public Hyundai(final int carId, final String carModel, final double carPrice){
+        this.carId=carId;
+        this.carModel=carModel;
+        this.carPrice=carPrice;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
+    @Override
+    double getResalePrice() {
+        return carPrice*0.60;
     }
+}
+
+ // Maruti class is created 
+
+class Maruti  extends Car {
+
+    public Maruti(final int carId, final String carModel, final double carPrice){
+        this.carId=carId;
+        this.carModel=carModel;
+        this.carPrice=carPrice;
+    }
+
+    @Override
+    double getResalePrice() {
+        return carPrice*0.80;
+    }
+}
+ // toyota class is created 
+
+
+ class Toyota  extends Car {
+
+    public Toyota(final int carId, final String carModel, final double carPrice){
+        this.carId=carId;
+        this.carModel=carModel;
+        this.carPrice=carPrice;
+    }
+
+    @Override
+    double getResalePrice() {
+        return carPrice*0.80;
+    }
+ }
+    
     
 
-
-} 
