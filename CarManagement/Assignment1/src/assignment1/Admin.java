22690bb330bf1e167e795fb1fc2dd764668bb657
Ravi kumar <ravikrrxl123@gmail.com>
2019-09-09 11:20:39 +0530

    Added My code

--- CarManagement/Assignment1/src/assignment1/Admin.java
+++ CarManagement/Assignment1/src/assignment1/Admin.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
  *
  * @author ravi
  */
-public class Assignment1 {
+public class Admin {
 
     /**
      * @param args the command line arguments
@@ -196,172 +196,19 @@ public class Assignment1 {
 
 }
 
-class Customer implements Comparable< Customer> {
-
-    public int id;
-    public String name;
-    public ArrayList<Car> carList;
-
-  public  Customer() {
-        Scanner obj1 = new Scanner(System.in);
-        Scanner obj2 = new Scanner(System.in);
-        System.out.println("Enter ID, Name");
-        id = obj1.nextInt();
-        name = obj2.next();
-        this.carList = new ArrayList<Car>();
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public ArrayList<Car> getCarList() {
-        return carList;
-    }
-
-    public void setCarList(ArrayList<Car> CarList) {
-        this.carList = CarList;
-    }
-    
-
-    public int compareTo(Customer o) {
-        //sort the arraylist by class object
-        int compareInt = this.name.compareTo(o.name);
-        if (compareInt < 0) {
-            return -1;//return this.Name is bigger
-        }
-        if (compareInt > 0) {
-            return 1;//return other.Name is bigger
-        }
-        return 0;//return they are equal
-    }
-    
-
-}
-// Car Class is as abstract class for different type of car
- abstract class Car {
-     public int carId;
-     public String carModel;
-     double carPrice;
-
-  
-    public int getCarId() {
-        return carId;
-    }
-
-    public double getCarPrice() {
-        return carPrice;
-    }
-
-    public String getCarModel() {
-        return carModel;
-    }
-    
-       abstract double getResalePrice();
 
 
- }
 
 
-// class is created for search the customer By id
-
-class SearchById {
-
-    public int cusId;
-
-    public SearchById() {
-        System.out.println("Enter Id Of Customer");
-        Scanner obj = new Scanner(System.in);
-        cusId = obj.nextInt();
-    }
-
-    public int getCusId() {
-        return cusId;
-    }
-
-}
 
 
-// this class created for distribute prize
-class RandomNumber {
 
-    public int num1;
-    public int num2;
-    public int num3;
-
-    public RandomNumber() {
-        System.out.println("Enter 3 random No as id:");
-        Scanner obj = new Scanner(System.in);
-        num1 = obj.nextInt();
-        num2 = obj.nextInt();
-        num3 = obj.nextInt();
-    }
-
-    public int getNum1() {
-        return num1;
-    }
-
-    public int getNum2() {
-        return num2;
-    }
-
-    public int getNum3() {
-        return num3;
-    }
-    
-    }
-
-  // Hyundai class is created 
-
-class Hyundai extends Car {
-
-    public Hyundai(final int carId, final String carModel, final double carPrice){
-        this.carId=carId;
-        this.carModel=carModel;
-        this.carPrice=carPrice;
-    }
-
-    @Override
-    double getResalePrice() {
-        return carPrice*0.60;
-    }
-}
-
- // Maruti class is created 
-
-class Maruti  extends Car {
-
-    public Maruti(final int carId, final String carModel, final double carPrice){
-        this.carId=carId;
-        this.carModel=carModel;
-        this.carPrice=carPrice;
-    }
-
-    @Override
-    double getResalePrice() {
-        return carPrice*0.80;
-    }
-}
- // toyota class is created 
+ 
 
 
- class Toyota  extends Car {
 
-    public Toyota(final int carId, final String carModel, final double carPrice){
-        this.carId=carId;
-        this.carModel=carModel;
-        this.carPrice=carPrice;
-    }
 
-    @Override
-    double getResalePrice() {
-        return carPrice*0.80;
-    }
- }
+ 
     
     
 
